05ede6e98e6aa6ccc2a4170e6cdf0945382a0954
Austin Wilson <wilsonwalrus@gmail.com>
2024-09-08 04:49:14 +0000

    Uploading first pass at the program

---
+++ fracturing.c
@@ -0,0 +1,114 @@
+//********************************************************
+// fracturing.c
+// Author: Austin Wilson, 
+// Class: COP 3223, Professor Parra
+// Purpose: This program calculates several aspects of a circle
+// based on user inputs.
+// Known bugs: calculateDistance does not output its 3rd line.
+// Output: (to the command line) The results of the calculations & the points entered by the user.
+// //********************************************************
+#include <math.h>
+#include <stdio.h>
+
+#define PI 3.14159
+
+//Asks for user input and returns the value.
+double askForUserInput()
+{
+	double userInput = 0;
+	scanf("%lf", &userInput);
+
+	return userInput;
+}
+
+//==========================================================================================================
+
+//Calculates the distance between two user-defined points.
+double calculateDistance()
+{
+	//Variables and Inputs
+	double x1 = 0, y1 = 0, x2 = 0, y2 = 0, distance = 0;
+	x1 = askForUserInput();
+	y1 = askForUserInput();
+	x2 = askForUserInput();
+	y2 = askForUserInput();
+
+	//Math time!
+	distance = pow(x2-x1, 2) + pow(y2-y1, 2);
+	distance = sqrt(distance);
+
+	//Outputs
+	printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
+	printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
+	return distance;
+}  
+//Calculates the perimeter of a circle.
+double calculatePerimeter()
+{
+    double perimeter = 1.0, diameter = 1.0;
+
+    //Math time!
+    diameter = calculateDistance();
+    perimeter = PI * diameter;
+    
+    //Output
+    printf("The perimeter of the city encompassed by your request is %lf\n", perimeter);
+
+    return 1;
+}
+
+
+//Calculates the area of a circle.
+double calculateArea()
+{
+    double area = 0.0, diameter = 0.0;
+
+    //Math time!
+    diameter = calculateDistance();
+    area =(PI * pow(diameter,2));
+    area = area / 4;
+
+    //Output
+    printf("The area of the city encompassed by your request is %lf\n", area);
+    return 1.8;
+}
+
+
+//Displays the width AKA diameter of a circle.
+double calculateWidth()
+{
+	double width = 1.0;
+    width = calculateDistance();
+
+    //Output
+    printf("The width of the city encompassed by your request is %lf\n", width);
+
+    return 1.0;
+}
+
+
+//Displays the height AKA also diameter of a circle.
+double calculateHeight()
+{
+	double height = 1.0;
+    height = calculateDistance();
+
+    //Output
+    printf("The height of the city encompassed by your request is %lf\n", height);
+
+    return 1.0;
+}
+
+//==========================================================================================================
+
+//The main function which calls & tests the previous functions.
+int main (int argc, char **argv)
+{
+	calculateDistance();
+	calculatePerimeter();
+	calculateArea ();
+	calculateWidth();
+	calculateHeight();
+
+	return 0;
+}
